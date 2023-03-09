78f43b6e1b8c9829d73bd20bf016eff4e286caa4
JotaJotaM1 <johnjames_89@hotmail.com>
2023-03-08 21:04:29 -0500

    finalizado

--- entregable-marzo23/src/App.jsx
+++ entregable-marzo23/src/App.jsx
@@ -1,12 +1,74 @@
+import { useState } from 'react'
+import Card from './Card'
+
 function App() {
-  //Aqui deberias agregar los estados y los handlers para los inputs
+  const [name, setName] = useState('')
+  const [comida, setComida] = useState('')
+  const [error, setError] = useState('')
+  const [comidaFav, setComidaFav] = useState({})
+  const [submitted, setSubmitted] = useState(false)
+
+
+  const handleSubmit = (e) =>{
+
+    e.preventDefault()
+
+    if (!name || !comida) {
+      setError('Porfavor rellena todos los campos')
+      return
+    }
+
+    if (name.length < 3){
+      setError(`longitud minima del campo name es de 3`)
+      return
+    }
+
+    if (comida.length < 6){
+      setError(`longitud minima del campo plato es de 6`)
+      return
+    }
+
+    name.trimStart();
+
+    const comidaFavorita = {
+      name: name,
+      comida: comida
+    }
+
+    setComidaFav(comidaFavorita)
+    setSubmitted(true)
+  }
 
   return (
-    <div className="App">
-      <h1>Elige un color</h1>
-      <form>{/* aqui deberias escribir tu codigo */}</form>
+    <div>
+      <h1>Elige tu comida favorita</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='name'>Nombre</label>
+          <input
+            type='text'
+            name='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor='imgUrl'>Comida favorito</label>
+          <input
+            type='text'
+            name='comida'
+            value={comida}
+            onChange={(e) => setComida(e.target.value)}
+          />
+        </div>
+        <button type='submit'>Adicionar Comida</button>
+      </form>
+      {error && <p>{error}</p>}
+
+      {submitted && <Card props={comidaFav} />}
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- entregable-marzo23/src/Card.jsx
+++ entregable-marzo23/src/Card.jsx
@@ -1,12 +1,13 @@
-//Este componente deberia recibir por props y mostrar en pantalla la informacion
-//que envia el usuario
+function Card(props) {
 
-function Card() {
-  return (
-    <div>
-      <h2>Esto es un componente</h2>
-    </div>
-  );
+  const { name, comida } = props.props;
+
+    return (
+      <div>
+        <h2>{`Hola ${name}`}</h2>
+        <p>{`Tu comida favorita es ${comida}`}</p>
+      </div>
+    );
 }
-
+  
 export default Card;
